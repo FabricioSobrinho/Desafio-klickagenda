7b03c7e70ad173339c0a4c77a8e85c2ad4309032
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-26 13:39:43 -0400

    First commit

---
+++ src/main/java/com/fabriciosobrinho/testetecnicoklickagenda/TestetecnicoklickagendaApplication.java
@@ -0,0 +1,13 @@
+package com.fabriciosobrinho.testetecnicoklickagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestetecnicoklickagendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestetecnicoklickagendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabriciosobrinho/testetecnicoklickagenda/TestetecnicoklickagendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabriciosobrinho.testetecnicoklickagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestetecnicoklickagendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
