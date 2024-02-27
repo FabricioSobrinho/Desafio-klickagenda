ebbd345fe5102471aa0d3bcfc3d27f19d370d555
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-27 14:16:39 -0400

    Feat: implement search routes

--- src/main/java/com/fabriciosobrinho/testetecnicoklickagenda/controllers/AvailableTimesController.java
+++ src/main/java/com/fabriciosobrinho/testetecnicoklickagenda/controllers/AvailableTimesController.java
@@ -1,6 +1,7 @@
 package com.fabriciosobrinho.testetecnicoklickagenda.controllers;
 
 import com.fabriciosobrinho.testetecnicoklickagenda.dtos.AvailableTimesDto;
+import com.fabriciosobrinho.testetecnicoklickagenda.exceptions.IlegalDateFormatException;
 import com.fabriciosobrinho.testetecnicoklickagenda.models.AvailableTimesModel;
 import com.fabriciosobrinho.testetecnicoklickagenda.repositories.AvailableTimeRepository;
 import jakarta.validation.Valid;
@@ -8,9 +9,9 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class AvailableTimesController {
@@ -19,10 +20,38 @@ public class AvailableTimesController {
     AvailableTimeRepository availableTimeRepository;
 
     @PostMapping("/horarios")
-    public ResponseEntity<AvailableTimesModel> saveNewTime(@RequestBody @Valid AvailableTimesDto availableTimesDto) {
+    public ResponseEntity<AvailableTimesModel> saveNewTime(@RequestBody @Valid AvailableTimesDto availableTimesDto) throws IlegalDateFormatException {
+        try {
+            availableTimesDto = AvailableTimesDto.create(
+                    availableTimesDto.horaDeInicio(),
+                    availableTimesDto.horaDeFim(),
+                    availableTimesDto.diaDaSemana(),
+                    availableTimesDto.nomeDoFuncionario()
+            );
+        } catch (Exception ex) {
+            throw new IlegalDateFormatException(ex.getMessage());
+        }
+
         var timeModel = new AvailableTimesModel();
         BeanUtils.copyProperties(availableTimesDto, timeModel);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(availableTimeRepository.save(timeModel));
     }
+
+    @GetMapping("/horarios")
+    public ResponseEntity<List<AvailableTimesModel>> GetAllTimes() {
+        return ResponseEntity.status(HttpStatus.OK).body(availableTimeRepository.findAll());
+    }
+
+    @GetMapping("/funcionario/{nomeFuncionario}")
+    public ResponseEntity<List<AvailableTimesModel>> getAvailableTimesByEmployeeName(@PathVariable(value = "nomeFuncionario") String nomeFuncionario) {
+        List<AvailableTimesModel> result = availableTimeRepository.findByNomeDoFuncionario(nomeFuncionario);
+
+        if (result.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
 }
