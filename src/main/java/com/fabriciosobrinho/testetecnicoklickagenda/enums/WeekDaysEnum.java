9498da7b6e272acd0b3bf20d4c2d9e61257637c9
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-27 14:04:25 -0400

    Feat: create week days enum

---
+++ src/main/java/com/fabriciosobrinho/testetecnicoklickagenda/enums/WeekDaysEnum.java
@@ -0,0 +1,11 @@
+package com.fabriciosobrinho.testetecnicoklickagenda.enums;
+
+public enum WeekDaysEnum {
+    Domingo,
+    SegundaFeira,
+    TercaFeira,
+    QuartaFeira,
+    QuintaFeira,
+    SextaFeira,
+    Sabado;
+}
