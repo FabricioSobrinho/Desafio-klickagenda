91b73c5a3b06910047a91101407d9f2187e3ed6c
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-27 15:07:40 -0400

    Feat: add Http Message Not Readable Exception

--- src/main/java/com/fabriciosobrinho/testetecnicoklickagenda/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/fabriciosobrinho/testetecnicoklickagenda/exceptions/GlobalExceptionHandler.java
@@ -3,16 +3,11 @@ package com.fabriciosobrinho.testetecnicoklickagenda.exceptions;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.time.format.DateTimeParseException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -42,5 +37,12 @@ public class GlobalExceptionHandler {
     public static ResponseEntity<String> handleValidationException(Exception ex) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         return ResponseEntity.status(status).body("Erro de validação: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    public static ResponseEntity<String> handleJSONParserException(Exception ex) {
+        HttpStatus status = HttpStatus.NOT_ACCEPTABLE;
+        return ResponseEntity.status(status).body("Erro na conversão: " + ex.getMessage());
     }
 }
