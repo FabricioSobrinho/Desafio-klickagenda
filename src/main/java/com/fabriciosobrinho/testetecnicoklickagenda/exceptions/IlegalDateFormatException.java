b8abeb87ee35c66be8ce3a8be1d2f100283f2941
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-27 14:06:52 -0400

    Feat: create custom date exception

---
+++ src/main/java/com/fabriciosobrinho/testetecnicoklickagenda/exceptions/IlegalDateFormatException.java
@@ -0,0 +1,7 @@
+package com.fabriciosobrinho.testetecnicoklickagenda.exceptions;
+public class IlegalDateFormatException extends Exception {
+    public IlegalDateFormatException(String message) {
+        super(message);
+    }
+}
+
