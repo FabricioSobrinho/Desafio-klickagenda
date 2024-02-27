676ea03dc327860b1ad6382a11cfcd930bfe2564
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-27 14:13:55 -0400

    Feat: add time validate

--- src/main/java/com/fabriciosobrinho/testetecnicoklickagenda/dtos/AvailableTimesDto.java
+++ src/main/java/com/fabriciosobrinho/testetecnicoklickagenda/dtos/AvailableTimesDto.java
@@ -1,10 +1,32 @@
 package com.fabriciosobrinho.testetecnicoklickagenda.dtos;
 
+import com.fabriciosobrinho.testetecnicoklickagenda.enums.WeekDaysEnum;
+import com.fabriciosobrinho.testetecnicoklickagenda.exceptions.IlegalDateFormatException;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.aspectj.lang.annotation.After;
 
 import java.time.LocalTime;
 
-public record AvailableTimesDto(@NotNull LocalTime horaDeInicio, @NotNull LocalTime horaDeFim, @NotBlank String diaDaSemana, @NotBlank String nomeDoFuncionario) {
+public record AvailableTimesDto(
+        @NotNull(message = "A hora de início não pode ser nula")
+        LocalTime horaDeInicio,
+        @NotNull(message = "A hora de fim não pode ser nula")
+        LocalTime horaDeFim,
+        @NotNull(message = "Insira um dia da semana.")
+        WeekDaysEnum diaDaSemana,
+        @NotBlank(message = "Insira o nome do funcionário")
+        String nomeDoFuncionario)
+{
+    public static AvailableTimesDto create(
+            @NotNull LocalTime horaDeInicio,
+            @NotNull LocalTime horaDeFim,
+            @NotNull WeekDaysEnum diaDaSemana,
+            @NotBlank String nomeDoFuncionario) {
+
+        if (horaDeFim.isBefore(horaDeInicio)) {
+            throw new IllegalArgumentException("A hora de fim deve ser maior que a hora de início");
+        }
+
+        return new AvailableTimesDto(horaDeInicio, horaDeFim, diaDaSemana, nomeDoFuncionario);
+    }
 }
