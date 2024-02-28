a6bafc3dfeae9ad6bcea427f84c69f826a5e9f62
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-28 15:26:30 -0400

    Feat: add navbar styles

--- Front-end-clickagenda/src/layouts/components/Navbar.jsx
+++ Front-end-clickagenda/src/layouts/components/Navbar.jsx
@@ -1,16 +1,21 @@
-import Button from "./forms components/Button"
-import SearchInput from "./forms components/SearchInput"
+import Button from "./forms components/Button";
+import SearchInput from "./forms components/SearchInput";
+
+import Styles from "../../styles/Navbar.module.css";
 
 function Navbar() {
   return (
-    <nav>
+    <nav className={Styles.mainNavbar}>
+      <div className={Styles.leftNavbar}>
         <Button text={"Adicionar horário"} />
         <Button text={"Mostrar horários"} />
-
-        <SearchInput text={"Digite o nome do funcionário"}/>
+      </div>
+      <div className={Styles.rightNavbar}>
+        <SearchInput text={"Digite o nome do funcionário"} />
         <Button text={"Buscar funcionário"} color={"blue"} minSize />
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
