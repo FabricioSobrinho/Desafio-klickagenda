4449f3c476f916d1ee3c7b8890e582784cb8ac73
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-28 14:48:07 -0400

    Feat: add button component and styles

---
+++ Front-end-clickagenda/src/layouts/components/forms components/Button.jsx
@@ -0,0 +1,16 @@
+import Styles from "../../../styles/Button.module.css";
+
+// eslint-disable-next-line react/prop-types
+function Button({ handleClick, color, text, minSize }) {
+  
+  return (
+    <button
+      onClick={handleClick}
+      className={`${Styles.button} ${color && Styles[color]} ${minSize && Styles.minSize}`}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default Button;
