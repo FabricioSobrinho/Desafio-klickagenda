8787c60686816a57f9224f5b54053ddac9ce262d
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-29 14:13:00 -0400

    Feat: add input component

---
+++ Front-end-clickagenda/src/layouts/components/forms components/Input.jsx
@@ -0,0 +1,29 @@
+import InputMask from "react-input-mask";
+
+// eslint-disable-next-line react/prop-types
+function Input({ text, mask, maskChar, heightInput, widthInput, type }) {
+  const inputStyle = {
+    height: `${heightInput}rem`,
+    width: `${widthInput}rem`,
+    background: "none",
+    padding: "1%",
+    outline: "none",
+    border: "solid .2rem #B3B3B3",
+    borderRadius: ".8rem",
+    fontSize: "1.8rem",
+    margin: "1rem 1rem",
+    boxShadow: "0px 0px 10px -5px #000000b2",
+  };
+
+  return (
+    <InputMask
+      style={inputStyle}
+      type={type}
+      mask={mask ? mask : ""}
+      maskChar={maskChar}
+      placeholder={text}
+    />
+  );
+}
+
+export default Input;
