434d4e38747f611519b25aec940d2d46ac83a205
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-29 14:11:57 -0400

    Refactor: minnor fixes

--- Front-end-clickagenda/src/layouts/components/forms components/Button.jsx
+++ Front-end-clickagenda/src/layouts/components/forms components/Button.jsx
@@ -2,11 +2,12 @@ import Styles from "../../../styles/Button.module.css";
 
 // eslint-disable-next-line react/prop-types
 function Button({ handleClick, color, text, minSize }) {
-  
   return (
     <button
       onClick={handleClick}
-      className={`${Styles.button} ${color && Styles[color]} ${minSize && Styles.minSize}`}
+      className={`${Styles.button} ${color && Styles[color]} ${
+        minSize && Styles.minSize
+      }`}
     >
       {text}
     </button>
