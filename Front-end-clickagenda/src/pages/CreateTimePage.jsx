c306f3eaf04539c4f4b5769e6abd6a726f304e91
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-29 14:31:58 -0400

    Feat: add background images

--- Front-end-clickagenda/src/pages/CreateTimePage.jsx
+++ Front-end-clickagenda/src/pages/CreateTimePage.jsx
@@ -1,14 +1,26 @@
 import Forms from "../layouts/components/Forms";
+
 import Styles from "../styles/CreateTimePage.module.css";
+
+import addTime from "../assets/public/images/AddTime.svg";
+import backIcon from "../assets/public/images/back-icon.png";
 
 function CreateTimePage() {
   return (
     <div className={Styles.mainCreatePage}>
-      <div className={Styles.leftCreateTime}></div>
+      <div className={Styles.leftCreateTime}>
+        <img src={addTime} alt="add time image" />
+      </div>
       <div className={Styles.forms}>
         <Forms />
       </div>
-      <div className={Styles.rightCreateTime}></div>
+      <div className={Styles.rightCreateTime}>
+        <img src={backIcon} alt="back ico image" className={Styles.img1} />
+        <img src={backIcon} alt="back ico image" className={Styles.img2} />
+        <img src={backIcon} alt="back ico image" className={Styles.img3} />
+        <img src={backIcon} alt="back ico image" className={Styles.img4} />
+        <img src={backIcon} alt="back ico image" className={Styles.img5} />
+      </div>
     </div>
   );
 }
