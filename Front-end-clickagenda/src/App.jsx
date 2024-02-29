6e8d31b5994e7ff573a704b109697dbe7f1cc70d
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-29 14:58:49 -0400

    Feat: add all times view route

--- Front-end-clickagenda/src/App.jsx
+++ Front-end-clickagenda/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./layouts/components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import CreateTimePage from "./pages/CreateTimePage";
+import ShowTimesPage from "./pages/ShowTimesPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<CreateTimePage />} />
+          <Route path="/horarios" element={<ShowTimesPage />} />
         </Routes>
       </Router>
     </main>
