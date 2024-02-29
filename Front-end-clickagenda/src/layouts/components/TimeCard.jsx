39b0a48272ca3b30f09aaa96477bf4be3dc6ce34
FabricioSobrinho <fabriciomails3@gmail.com>
2024-02-29 15:36:00 -0400

    Feat: add edit and delete buttons

--- Front-end-clickagenda/src/layouts/components/TimeCard.jsx
+++ Front-end-clickagenda/src/layouts/components/TimeCard.jsx
@@ -1,6 +1,10 @@
 import Styles from "../../styles/TimeCard.module.css";
 
-function TimeCard() {
+import { FaPencilAlt, FaTrashAlt } from "react-icons/fa";
+import Button from "./forms components/Button";
+
+// eslint-disable-next-line react/prop-types
+function TimeCard({ enableEdit }) {
   return (
     <div className={Styles.mainCard}>
       <div className={Styles.title}>NOME DO USUARIO</div>
@@ -13,6 +17,12 @@ function TimeCard() {
         <p>Seg: 12h22 à 12h29</p>
         <p>Seg: 12h22 à 12h29</p>
       </div>
+      {enableEdit && (
+        <div className={Styles.editTime}>
+          <Button text={<FaPencilAlt />} minSize/>
+          <Button text={<FaTrashAlt />} color={"red"} minSize/>
+        </div>
+      )}
     </div>
   );
 }
