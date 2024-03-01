31a940a37304714134b253f31de0d9968bd9e635
FabricioSobrinho <fabriciomails3@gmail.com>
2024-03-01 10:54:53 -0400

    Feat: implement CORS

--- Back-end/src/main/java/com/fabriciosobrinho/testetecnicoklickagenda/controllers/AvailableTimesController.java
+++ Back-end/src/main/java/com/fabriciosobrinho/testetecnicoklickagenda/controllers/AvailableTimesController.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class AvailableTimesController {
 
