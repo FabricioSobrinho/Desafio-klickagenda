57e5d856ee8961eb19db06cf064e49b995519ca6
FabricioSobrinho <fabriciomails3@gmail.com>
2024-03-01 15:47:44 -0400

    Feat: add search function

--- Front-end-clickagenda/src/layouts/components/Navbar.jsx
+++ Front-end-clickagenda/src/layouts/components/Navbar.jsx
@@ -3,7 +3,37 @@ import SearchInput from "./forms components/SearchInput";
 
 import Styles from "../../styles/Navbar.module.css";
 
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Navbar() {
+  const [employeeName, setEmployeeName] = useState(null);
+  const navigate = useNavigate(); 
+
+  const handleEmployeeName = (e) => {
+    setEmployeeName(e.target.value);
+  };
+
+  const searchEmployee = async () => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/horarios/${employeeName}`,
+        config
+      );
+
+      console.log(response);
+      navigate(`/funcionario/${employeeName}`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <nav className={Styles.mainNavbar}>
       <div className={Styles.leftNavbar}>
@@ -11,8 +41,17 @@ function Navbar() {
         <Button text={"Mostrar horários"} />
       </div>
       <div className={Styles.rightNavbar}>
-        <SearchInput text={"Digite o nome do funcionário"} />
-        <Button text={"Buscar funcionário"} color={"blue"} minSize />
+        <SearchInput
+          text={"Digite o nome do funcionário"}
+          value={employeeName}
+          handleChange={handleEmployeeName}
+        />
+        <Button
+          text={"Buscar funcionário"}
+          color={"blue"}
+          handleClick={searchEmployee}
+          minSize
+        />
       </div>
     </nav>
   );
