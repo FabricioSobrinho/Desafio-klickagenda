6ae8addfcf2ed740848b26c420c2f71088010cd8
FabricioSobrinho <fabriciomails3@gmail.com>
2024-03-01 11:00:40 -0400

    Feat: implement add time connection function

--- Front-end-clickagenda/src/pages/CreateTimePage.jsx
+++ Front-end-clickagenda/src/pages/CreateTimePage.jsx
@@ -1,4 +1,8 @@
+import axios from "axios";
+import { useState } from "react";
+
 import Forms from "../layouts/components/Forms";
+import Message from "../layouts/components/Message";
 
 import Styles from "../styles/CreateTimePage.module.css";
 
@@ -6,13 +10,66 @@ import addTime from "../assets/public/images/AddTime.svg";
 import backIcon from "../assets/public/images/back-icon.png";
 
 function CreateTimePage() {
+  const [message, setMessage] = useState(false);
+  const [type, setType] = useState("success");
+
+  const [timeData, setTimeData] = useState({
+    diaDaSemana: "SegundaFeira",
+  });
+
+  const handleTime = (e) => {
+    setTimeData((prevTimeData) => ({
+      ...prevTimeData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleWeekDay = (e) => {
+    setTimeData({
+      ...timeData,
+      diaDaSemana: e.target.options[e.target.selectedIndex].value,
+    });
+  };
+
+  const createTime = async () => {
+    setMessage("");
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/horario",
+        timeData,
+        config
+      );
+
+      console.log(response);
+      setType("success");
+      setMessage("Horario criado com sucesso!");
+    } catch (err) {
+      console.log(err);
+      setType("error");
+      setMessage("Erro ao criar horario!");
+    }
+  };
+
   return (
     <div className={Styles.mainCreatePage}>
+      {message && <Message type={type} time={2000} text={message} />}
       <div className={Styles.leftCreateTime}>
         <img src={addTime} alt="add time image" />
       </div>
       <div className={Styles.forms}>
-        <Forms />
+        <Forms
+          handleInput={handleTime}
+          handleSelect={handleWeekDay}
+          selectValue={timeData.diaDaSemana}
+          action={createTime}
+        />
       </div>
       <div className={Styles.rightCreateTime}>
         <img src={backIcon} alt="back ico image" className={Styles.img1} />
