461332086410878dd8cbccbefee9207c5590821b
FabricioSobrinho <fabriciomails3@gmail.com>
2024-03-01 10:56:38 -0400

    Feat: add duplicate times exception

--- Back-end/src/main/java/com/fabriciosobrinho/testetecnicoklickagenda/exceptions/GlobalExceptionHandler.java
+++ Back-end/src/main/java/com/fabriciosobrinho/testetecnicoklickagenda/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.fabriciosobrinho.testetecnicoklickagenda.exceptions;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,5 +30,15 @@ public class GlobalExceptionHandler {
     public static ResponseEntity<String> handleJSONParserException(Exception ex) {
         HttpStatus status = HttpStatus.NOT_ACCEPTABLE;
         return ResponseEntity.status(status).body("Erro na conversão: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        if (ex.getCause() instanceof ConstraintViolationException) {
+            return ResponseEntity.badRequest().body("Erro: Já existe um horário cadastrado para o mesmo funcionário, dia da semana e horário.");
+        }
+
+        return ResponseEntity.badRequest().body("Erro de integridade de dados." + ex.getMessage());
     }
 }
