22c494e6eea2da2bbd21758f1368d163ae0c62fd
FabricioSobrinho <fabriciomails3@gmail.com>
2024-03-01 15:07:33 -0400

    Refacor: add suport to iterable items

--- Front-end-clickagenda/src/layouts/components/TimeCard.jsx
+++ Front-end-clickagenda/src/layouts/components/TimeCard.jsx
@@ -1,26 +1,33 @@
 import Styles from "../../styles/TimeCard.module.css";
-
 import { FaPencilAlt, FaTrashAlt } from "react-icons/fa";
 import Button from "./forms components/Button";
 
 // eslint-disable-next-line react/prop-types
-function TimeCard({ enableEdit }) {
+function TimeCard({ enableEdit, nomeDoFuncionario, data }) {
   return (
     <div className={Styles.mainCard}>
-      <div className={Styles.title}>NOME DO USUARIO</div>
-      <div className={Styles.times}>
-        <p>Seg: 12h22 à 12h29</p>
-        <p>Seg: 12h22 à 12h29</p>
-        <p>Seg: 12h22 à 12h29</p>
-        <p>Seg: 12h22 à 12h29</p>
-        <p>Seg: 12h22 à 12h29</p>
-        <p>Seg: 12h22 à 12h29</p>
-        <p>Seg: 12h22 à 12h29</p>
-      </div>
+      <div className={Styles.title}>{nomeDoFuncionario}</div>
+      {data.map((item, index) => (
+        <div key={index}>
+          <div className={Styles.times}>
+            <p>
+              {item.diaDaSemana == "SegundaFeira" && "Segunda Feira"}
+              {item.diaDaSemana == "TercaFeira" && "Terca Feira"}
+              {item.diaDaSemana == "QuartaFeira" && "Quarta Feira"}
+              {item.diaDaSemana == "QuintaFeira" && "Quinta Feira"}
+              {item.diaDaSemana == "SextaFeira" && "Sexta Feira"}
+              {item.diaDaSemana == "Sabado" && "Sábado"}
+              {item.diaDaSemana == "Domingo" && "Domingo"}
+              : {item.horaDeInicio} às {item.horaDeFim}
+            </p>
+          </div>
+        </div>
+      ))}
+
       {enableEdit && (
         <div className={Styles.editTime}>
-          <Button text={<FaPencilAlt />} minSize/>
-          <Button text={<FaTrashAlt />} color={"red"} minSize/>
+          <Button text={<FaPencilAlt />} minSize />
+          <Button text={<FaTrashAlt />} color={"red"} minSize />
         </div>
       )}
     </div>
