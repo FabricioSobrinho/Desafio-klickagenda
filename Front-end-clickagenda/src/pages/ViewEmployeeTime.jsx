d976da85ed62c040c21c129ed7f45f4eb6a85b07
FabricioSobrinho <fabriciomails3@gmail.com>
2024-03-01 15:49:26 -0400

    Feat: start implement of separate view time

--- Front-end-clickagenda/src/pages/ViewEmployeeTime.jsx
+++ Front-end-clickagenda/src/pages/ViewEmployeeTime.jsx
@@ -1,13 +1,15 @@
 import Styles from "../styles/ViewEmployeeTime.module.css";
 
 import TimeCard from "../layouts/components/TimeCard";
+import { useParams } from "react-router-dom";
 
 function ViewEmployeeTime() {
+  const { username } = useParams();
+
+  console.log(username);
   return (
     <div className={Styles.mainViewTime}>
       <div className={Styles.times}>
-        <TimeCard enableEdit />
-        <TimeCard enableEdit />
         <TimeCard enableEdit />
       </div>
     </div>
