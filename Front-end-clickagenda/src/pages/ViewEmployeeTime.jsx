78a563def100c4456c0f9d0a622b35a5de68e202
FabricioSobrinho <fabriciomails3@gmail.com>
2024-03-02 14:21:17 -0400

    Feat: implement view employee time

--- Front-end-clickagenda/src/pages/ViewEmployeeTime.jsx
+++ Front-end-clickagenda/src/pages/ViewEmployeeTime.jsx
@@ -3,14 +3,45 @@ import Styles from "../styles/ViewEmployeeTime.module.css";
 import TimeCard from "../layouts/components/TimeCard";
 import { useParams } from "react-router-dom";
 
-function ViewEmployeeTime() {
-  const { username } = useParams();
+import imageLeft from "../assets/public/images/SearchTime.svg";
+import imageRight from "../assets/public/images/SearchTime2.svg";
 
-  console.log(username);
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function ViewEmployeeTime() {
+  const { employee } = useParams();
+  const [employeeData, setEmployeeData] = useState([]);
+
+  useEffect(() => {
+    async function getEmployee() {
+      setEmployeeData([]);
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/funcionario/${employee}`,
+          config
+        );
+
+        setEmployeeData(() => [...response.data]);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    getEmployee();
+  }, []);
+
   return (
     <div className={Styles.mainViewTime}>
+      <img src={imageLeft} className={Styles.leftImg}/>
+      <img src={imageRight} className={Styles.rightImg}/>
       <div className={Styles.times}>
-        <TimeCard enableEdit />
+        <TimeCard data={employeeData} nomeDoFuncionario={employee} />
       </div>
     </div>
   );
